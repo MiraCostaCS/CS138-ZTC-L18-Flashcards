7360d9c441e875c726a442e00b70c48e9cac57ec
kevinmlewis <kevinmlewis@gmail.com>
2026-08-15 15:09:36 -0700

    Initialize main.py with program structure
    Added initial structure and docstring for main.py.

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+DEVELOPER: <your name>
+COLLABORATORS: <anyone you worked with>
+'''
+"""A one line summary of the program, terminated by a period.
+Leave one blank line. The rest of this docstring should contain an
+overall description of the program.
+"""
+##########################################
+# IMPORTS:
+##########################################
+from tkinter import *
+##########################################
+# CLASS DEFINITIONS:
+##########################################
+#<replace this line with your page classes, e.g. MenuPage, EditPage, StudyPage>
+##########################################
+# MAIN PROGRAM:
+##########################################
+#<replace this line with your root = Tk() setup, widgets, and root.mainloop()>
